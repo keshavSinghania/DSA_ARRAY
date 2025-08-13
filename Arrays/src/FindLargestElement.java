77d535e4ad7a697fe957137211dec755d81bed7c
Keshav Singhania <worksinghania@gmail.com>
2025-08-13 23:18:41 +0530

    FInd the largest element in array

---
+++ Arrays/src/FindLargestElement.java
@@ -0,0 +1,15 @@
+public class FindLargestElement {
+}
+
+//https://www.geeksforgeeks.org/problems/largest-element-in-array4009/0?utm_source=chatgpt.com
+//class Solution {
+//    public static int largest(int[] arr) {
+//        int largest = arr[0];
+//        for( int i = 1 ; i<arr.length; i++){
+//            if(largest<arr[i]){
+//                largest = arr[i];
+//            }
+//        }
+//        return largest;
+//    }
+//}
