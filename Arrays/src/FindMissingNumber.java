51d67044617e312ac058680c1ed1c627df59ad76
Keshav Singhania <worksinghania@gmail.com>
2025-08-14 12:28:57 +0530

    Find Missing Number

---
+++ Arrays/src/FindMissingNumber.java
@@ -0,0 +1,15 @@
+public class FindMissingNumber {
+}
+
+//https://leetcode.com/problems/missing-number/
+//class Solution {
+//    public int missingNumber(int[] nums) {
+//        int n = nums.length;
+//        int totalSum = (n*(n+1))/2;
+//        int mySum =0;
+//        for(int i = 0; i<n; i++){
+//            mySum+=nums[i];
+//        }
+//        return (totalSum - mySum);
+//    }
+//}
