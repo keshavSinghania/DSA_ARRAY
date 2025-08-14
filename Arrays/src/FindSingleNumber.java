6a5e3fb8b7b3eb87d8b11489dde934d006a712fd
Keshav Singhania <worksinghania@gmail.com>
2025-08-14 12:46:54 +0530

    find single number

---
+++ Arrays/src/FindSingleNumber.java
@@ -0,0 +1,13 @@
+public class FindSingleNumber {
+}
+//https://leetcode.com/problems/single-number/submissions/1734649175/
+//class Solution {
+//    public int singleNumber(int[] nums) {
+//        int xor=0;
+//
+//        for(int i =0; i<nums.length; i++){
+//            xor = xor^nums[i];
+//        }
+//        return xor;
+//    }
+//}
