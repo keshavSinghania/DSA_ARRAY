70573fc982f7383c1d947377f868f4665779331a
Keshav Singhania <worksinghania@gmail.com>
2025-08-18 19:37:32 +0530

    find the majority element in array

---
+++ Arrays/src/MajorityElement.java
@@ -0,0 +1,37 @@
+public class MajorityElement {
+}
+//class Solution {
+//    public int majorityElement(int[] nums) {
+//        int ele=0;
+//        int count=0;
+//        for(int i = 0; i<nums.length; i++){
+//            if(count == 0){
+//                ele = nums[i];
+//                count++;
+//            }else{
+//                if(nums[i]==ele){
+//                    count++;
+//                }else{
+//                    count--;
+//                }
+//            }
+//        }
+//        if(isAns(nums,ele)){
+//            return ele;
+//        }else{
+//            return -1;
+//        }
+//    }
+//
+//    //fucntion to check wheather it is ans or not
+//    boolean isAns(int[] nums, int n){
+//        int count = 0;
+//        for(int i = 0; i<nums.length; i++){
+//            if(n==nums[i]){
+//                count++;
+//            }
+//            if(count>nums.length/2) return true;
+//        }
+//        return false;
+//    }
+//}
